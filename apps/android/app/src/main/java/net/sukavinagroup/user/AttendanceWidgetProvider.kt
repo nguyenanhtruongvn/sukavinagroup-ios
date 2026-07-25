4b5e66119a5847f1677a15795bf45bf65b8111fc
Sukavina Build <nguyenanhtruongvn@gmail.com>
2026-07-25 11:37:02 +0700

    fix: restore Android widget time parser

--- apps/android/app/src/main/java/net/sukavinagroup/user/AttendanceWidgetProvider.kt
+++ apps/android/app/src/main/java/net/sukavinagroup/user/AttendanceWidgetProvider.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import net.sukavinagroup.user.data.ApiClient
 import net.sukavinagroup.user.data.Dashboard
+import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
